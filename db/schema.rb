afb763147f1b857bd6576257ad7f0cdf0025fb28
OlegKrutas <krutas_1990@mail.ru>
2014-02-26 20:42:37 +0200

    ScrumBoard

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140226103616) do
+ActiveRecord::Schema.define(version: 20140226125559) do
 
   create_table "projects", force: true do |t|
     t.string   "title"
@@ -29,6 +29,15 @@ ActiveRecord::Schema.define(version: 20140226103616) do
     t.string   "project_id"
     t.string   "status"
     t.string   "sprint_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "tasks", force: true do |t|
+    t.string   "title"
+    t.string   "description"
+    t.string   "status"
+    t.integer  "story_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
